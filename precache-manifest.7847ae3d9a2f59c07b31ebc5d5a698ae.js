9bc01db8ceabdf683feed5dd558eb315363bb95e
Martin Viksten <vikstenmartin@gmail.com>
2020-10-03 12:39:57 +0100

    Updates

---
+++ precache-manifest.7847ae3d9a2f59c07b31ebc5d5a698ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfd91b615d095fb4c03a63c6e7ce366",
+    "url": "/Card-Generator/index.html"
+  },
+  {
+    "revision": "acfc81a3f8a3a5aee349",
+    "url": "/Card-Generator/static/css/2.fc7caa24.chunk.css"
+  },
+  {
+    "revision": "5346072fba1d8941940d",
+    "url": "/Card-Generator/static/css/main.fc9a15b3.chunk.css"
+  },
+  {
+    "revision": "acfc81a3f8a3a5aee349",
+    "url": "/Card-Generator/static/js/2.59e578a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Card-Generator/static/js/2.59e578a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5346072fba1d8941940d",
+    "url": "/Card-Generator/static/js/main.f5d6ee69.chunk.js"
+  },
+  {
+    "revision": "94361811fabea62991bc",
+    "url": "/Card-Generator/static/js/runtime-main.4aea36ed.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/Card-Generator/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/Card-Generator/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "af24ba59bf4cbd034a20c5415cf57653",
+    "url": "/Card-Generator/static/media/dog.af24ba59.jpg"
+  },
+  {
+    "revision": "56c9730f62bf7d2c8885f05c99d159e7",
+    "url": "/Card-Generator/static/media/dog2.56c9730f.jpeg"
+  },
+  {
+    "revision": "c3f141f5a11575b95097f4befe122979",
+    "url": "/Card-Generator/static/media/dog3.c3f141f5.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/card-generator/precache-manifest.fbbf4ce1f21cac817f17310c28701f90.js"
+  "/Card-Generator/precache-manifest.7847ae3d9a2f59c07b31ebc5d5a698ae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/card-generator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Card-Generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
